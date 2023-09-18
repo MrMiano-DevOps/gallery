f870197185954a780175c57a77acb9877f47c529
MrMiano-DevOps <109775336+MrMiano-DevOps@users.noreply.github.com>
2023-09-18 13:00:29 +0300

    Test (#1)
    * changes
    
    * procfile
    
    * db name
    
    * Add a test file
    
    * config file for the database
    
    * Add new dependencies
    
    * add config
    
    * database config
    
    * Update _config.js
    
    Add URL to production DB
    
    * Update serverTest.js
    
    Extend timeout for tests to work on Jenkins
    
    * Update package.json
    
    add exit flag on mocha tests
    
    * Update _config.js
    
    * Update _config.js
    
    ---------
    
    Co-authored-by: john mutavi <jonnygovish@gmail.com>
    Co-authored-by: Brian Marete <bmarete10@gmail.com>

--- server.js
+++ server.js
@@ -2,12 +2,17 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const path = require('path');
+const config = require('./_config');
 
 // Define routes
 let index = require('./routes/index');
 let image = require('./routes/image');
 
+// Initializing the app
+const app = express();
+
 // connecting the database
+
 // let mongodb_url = 'mongodb://0.0.0.0:27017/';
 // let dbName = 'darkroom';
 mongoose.connect('mongodb+srv://anthonymaina:SrMbekz2Dx6rC1tm@myappcluster.llfcrzn.mongodb.net/?retryWrites=true&w=majority',{ useNewUrlParser: true , useUnifiedTopology: true }, (err)=>{
@@ -15,13 +20,12 @@ mongoose.connect('mongodb+srv://anthonymaina:SrMbekz2Dx6rC1tm@myappcluster.llfcr
 });
 
 // test if the database has connected successfully
-let db = mongoose.connection;
-db.once('open', ()=>{
-    console.log('Database connected successfully')
-})
+// let db = mongoose.connection;
+// db.once('open', ()=>{
+//     console.log('Database connected successfully')
+// })
 
-// Initializing the app
-const app = express();
+
 
 
 // View Engine
@@ -44,3 +48,6 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT,() =>{
     console.log(`Server is listening at http://localhost:${PORT}`)
 });
+
+
+module.exports = app;
